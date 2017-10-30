7cf843181ba73d84de8e1a51f74fa7a07e0052fb
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2017-10-30 19:52:31 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -69,7 +69,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts season.to_s.capitalize + ":"
     holiday.each do |holiday, supplies|
-      puts "  " + holiday.to_s.capitalize + ": " + supplies.join(", ")
+      puts "  " + holiday.to_s.split('_').map(&:capitalize).join(' ') + ": " + supplies.join(", ")
     end
   end
 
